aeb1d91a8491baf0a101e9c54ebd62851453601b
gsxhnd <gsxhnd@gmail.com>
2021-08-05 18:27:29 +0800

    update

--- app/main/electron.ts
+++ app/main/electron.ts
@@ -1,11 +1,12 @@
 import { app, BrowserWindow } from "electron";
+import path from "path";
 
 function createWindow() {
-  let win = new BrowserWindow({ width: 600, height: 800, frame:false });
+  let win = new BrowserWindow({ width: 600, height: 800, frame: false });
   console.log(process.versions.chrome);
   console.log(process.versions.electron);
 
-  win.loadFile("../renderer/index.html");
+  win.loadFile(path.join(__dirname, "../renderer/index.html"));
 }
 
 app.on("ready", createWindow);
