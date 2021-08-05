6bdc1e51a1cb7d7be045ca296ef1bdda01099a40
gsxhnd <gsxhnd@gmail.com>
2021-08-06 00:32:11 +0800

    update

--- app/main/electron.ts
+++ app/main/electron.ts
@@ -1,12 +1,23 @@
 import { app, BrowserWindow } from "electron";
+import ElectronStore from "electron-store";
 import path from "path";
 
-function createWindow() {
-  let win = new BrowserWindow({ width: 600, height: 800, frame: false });
-  console.log(process.versions.chrome);
-  console.log(process.versions.electron);
+let store = new ElectronStore();
 
+function createWindow() {
+  store.set("dev_mode", app.isPackaged ? false : true);
+  store.set("lib_list", ["a", "n"]);
+  console.log(app.isPackaged);
+  console.log(app.getLocale());
+  console.log(app.getPath("userData"));
+  let win = new BrowserWindow({ width: 600, height: 800, frame: false });
   win.loadFile(path.join(__dirname, "../renderer/index.html"));
 }
 
+function loadConfig() {
+  let language = store.get("language");
+  console.log(language);
+}
+
+app.on("will-finish-launching", loadConfig);
 app.on("ready", createWindow);
